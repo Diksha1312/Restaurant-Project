679b432f43f370eb225bb269ba9cbdb84e6bb252
Diksha1312 <gdiksha1312@gmail.com>
2025-06-17 14:19:06 +0200

    update routes for login page and add header page

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,16 @@
 <template>
+    <HeaderPage />
     <h1>Welcome to Your Vue.js App</h1>
     <img alt="Vue logo" src="../assets/logo.png">
 </template>
 
 <script>
+import HeaderPage from './header.vue';
 export default {
     name: 'HomePage',
+    components: {
+        HeaderPage
+    },
     mounted() {
         const user = localStorage.getItem('user');
         if (!user) {
