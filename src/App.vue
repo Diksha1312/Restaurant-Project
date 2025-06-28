a65b3bc09e3a57358d7d4e5947c4579574cf78ce
Diksha1312 <gdiksha1312@gmail.com>
2025-06-28 14:29:44 +0200

    update add.vue with form and routes

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
     width: 200px;
 }
 
-.register input, .login input{
+.register input, .login input, .add-restaurant-form input {
     width: 300px;
     height: 40px;
     padding-left: 20px;
@@ -31,7 +31,7 @@ export default {
     border: 1px solid skyblue;
 }
 
-.register button, .login button{
+.register button, .login button, .add-restaurant-form button {
     width: 320px;
     height: 40px;
     border: 1px solid skyblue;
--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,15 +1,51 @@
 <template>
     <HeaderPage />
     <h1>Add Restaurant</h1>
-    <img alt="Vue logo" src="../assets/logo.png">
+    <form class="add-restaurant-form">
+        <input type="text" name="name" placeholder="Restaurant Name" v-model="restaurant.restaurantName" />
+        <input type="text" name="cuisine" placeholder="Cuisine Type" v-model="restaurant.cuisineType" />
+        <input type="text" name="location" placeholder="Location" v-model="restaurant.location" />
+        <button type="button" v-on:click="addRestaurant">Add Restaurant</button>
+    </form>
 </template>
 
 <script>
 import HeaderPage from './header.vue';
+import axios from 'axios';
 export default {
     name: 'AddPage',
     components: {
-        HeaderPage
+        HeaderPage 
+    },
+    data() {
+        return {
+            restaurant : {
+                restaurantName: '',
+                cuisineType: '',
+                location: ''
+            }
+        };
+    },
+    methods: {
+        async addRestaurant() {
+            console.log('Restaurant added:', this.restaurant);
+            const result = await axios.post('http://localhost:3000/restaurants', {
+                name: this.restaurant.restaurantName,
+                cuisine: this.restaurant.cuisineType,
+                location: this.restaurant.location
+            });
+            console.warn('Result:', result);
+            if (result.status === 201) {
+                this.$router.push({name: 'HomePage'});
+            } else {
+                console.error('Error adding restaurant:', result);
+            }
+            this.restaurant = {
+                restaurantName: '',
+                cuisineType: '',
+                location: ''
+            };
+        }
     },
     mounted() {
         const user = localStorage.getItem('user');
